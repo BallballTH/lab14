85b42f2c1b362ae9f5d08a993ff9eaffa896656f
BallChon <Buranin_b@cmu.ac.th>
2024-01-05 16:47:21 +0700

    lab14 completed wow

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,7 +3,18 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+		for(int i=1; i<N;i++){
+		T temp = d[i];
+		int j=i-1; 
+		while(j >= 0 && d[j] < temp){
+			d[j + 1] = d[j];
+			 j--;
+		}
+		d[j + 1] = temp;
+		cout << "Pass " << i << ":";
+	    for(int f = 0; f < 10; f++) cout << d[f] << " ";
+		cout << endl;
+	}
 }
 
 int main(){
